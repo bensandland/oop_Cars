33574bf3e66d89064e7a1a63e250e7859a8948e1
Danny Haslund <dannyhasl1@gmail.com>
2017-11-20 13:44:48 +0100

    fixed

--- OOP_Cars/Form1.cs
+++ OOP_Cars/Form1.cs
@@ -12,10 +12,20 @@ namespace OOP_Cars
 {
     public partial class Form1 : Form
     {
-        Program pro;
+        List<int> empid = new List<int> { };
         public Form1()
         {
             InitializeComponent();
+
+            empid.Add(1);
+            empid.Add(2);
+            empid.Add(3);
+
+            int id = empid.Count;
+            for (int i = 1; i < id + 1; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -25,7 +35,6 @@ namespace OOP_Cars
 
         public void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            int id = pro.EmpIDLengh();
             
         }
     }
--- OOP_Cars/Program.cs
+++ OOP_Cars/Program.cs
@@ -6,28 +6,24 @@ using System.Windows.Forms;
 
 namespace OOP_Cars
 {
-    public class Program
+    public static class Program
     {
-        List<int> empid = new List<int> { };
+        
 
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        public void Main()
+        public static void Main()
         {
             Employees emp1 = new Employees(1, "Kurt", 16.45);
 
             
-            empid.Add(1);
+            
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-        }
-        public int EmpIDLengh()
-        {
-            return empid.Count;
         }
     }
 }
