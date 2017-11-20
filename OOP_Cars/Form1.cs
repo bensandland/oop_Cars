a25f25e9913ac956f95de2853fa842f06f04f871
Danny Haslund <dannyhasl1@gmail.com>
2017-11-20 14:13:14 +0100

    üpdate form

--- OOP_Cars/Form1.cs
+++ OOP_Cars/Form1.cs
@@ -12,25 +12,17 @@ namespace OOP_Cars
 {
     public partial class Form1 : Form
     {
-        List<int> empid = new List<int> { };
+        
+        List<Employees> employess;
         public Form1()
         {
             InitializeComponent();
 
-            empid.Add(1);
-            empid.Add(2);
-            empid.Add(3);
-
-            int id = empid.Count;
-            for (int i = 1; i < id + 1; i++)
-            {
-                comboBox1.Items.Add(i);
-            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show(comboBox1.SelectedItem);
+            //MessageBox.Show(comboBox1.SelectedItem);
         }
 
         public void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
